86b6d3509d858ea91bf44ba6b4f690256e5c1a0c
wesmnovaes <wes.mnovaes@gmail.com>
2017-09-16 23:40:52 -0300

    Atividade Lpweb - Crud Vingadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,31 @@ import { ListaHeroisComponent } from './lista-herois/lista-herois.component'
 })
 export class AppComponent implements OnInit {
   novo: Vingador = new Vingador(0, '', '','');
-  t: ListaHeroisComponent;
-  constructor(){  
-  }
+  title: string;
+  vingadores: Array<Vingador>;
+  selecionado: Vingador;
+  constructor(){
 
-  cadastrar(nome: string, pessoa: string, url: string){
-    this.novo = new Vingador(1,nome,pessoa,url);
-    this.t.add(this.novo);
-  }
-  
+    this.title = 'Vingadores';
+    this.vingadores = [
+        new Vingador(1, 'Capitão América', 'Steve Rogers','www.capitão.com'),
+        new Vingador(2, 'Viúva Negra', 'Natasha Romanoff',''),
+        new Vingador(3, 'Ms. Marvel', 'Carol Danvers',''),
+        new Vingador(4, 'Deadpool', 'Wade Wilson',''),
+        new Vingador(5, 'Gavião Arqueiro', 'Clint Barton','')
+    ]; 
+}
+cadastrar(): void{
+  const novoId: number = this.vingadores.length + 1;
+  this.vingadores.push(new Vingador(novoId, this.novo.nome, this.novo.pessoa,this.novo.url));
+  this.novo = new Vingador(0, '', '','');
+}
+deletar(vingador: Vingador): void{
+  this.vingadores.splice(vingador.id,1); 
+}
+editar(vingador: Vingador): void{
+  this.novo = vingador;
+}
   ngOnInit(): void {
   }
 }
--- src/app/lista-herois/lista-herois.component.ts
+++ src/app/lista-herois/lista-herois.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Vingador } from '../vingador';
 
 
@@ -8,26 +8,7 @@ import { Vingador } from '../vingador';
   styleUrls: ['./lista-herois.component.css']
 })
 export class ListaHeroisComponent implements OnInit {
-  @Input() heroi: Vingador;
 
-  title: string;
-  vingadores: Array<Vingador>;
-  selecionado: Vingador;
-  
-  constructor(){
-
-    this.title = 'Vingadores';
-    this.vingadores = [
-        new Vingador(1, 'Capitão América', 'Steve Rogers',''),
-        new Vingador(2, 'Viúva Negra', 'Natasha Romanoff',''),
-        new Vingador(3, 'Ms. Marvel', 'Carol Danvers',''),
-        new Vingador(4, 'Deadpool', 'Wade Wilson',''),
-        new Vingador(5, 'Gavião Arqueiro', 'Clint Barton','')
-    ]; 
-}
-add(vingador: Vingador){
-  this.vingadores.push(vingador);
-}
 ngOnInit() {
 }
 }
